d2c2efd767b11ffb650ec6509b84419d841867c4
Yuto Takahashi <ytaka23dev@gmail.com>
2023-03-26 01:44:58 +0900

    Add basic ASTs

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,60 @@ module Main exposing (greet, main)
 
 import Browser
 import Html exposing (Html, text)
+import List.Nonempty exposing (Nonempty)
+
+
+type alias Name =
+    String
+
+
+type Variable
+    = Variable Name
+
+
+type Value
+    = NumberVal Int
+    | StringVal String
+
+
+type ArithExp
+    = Lit Value
+    | Var Name
+    | Plus ArithExp ArithExp
+    | Minus ArithExp ArithExp
+    | Times ArithExp ArithExp
+    | Div ArithExp ArithExp
+    | Percent ArithExp ArithExp
+
+
+type BoolExp
+    = Eq ArithExp ArithExp
+    | Neq ArithExp ArithExp
+    | GT ArithExp ArithExp
+    | GE ArithExp ArithExp
+    | LE ArithExp ArithExp
+    | LT ArithExp ArithExp
+    | And BoolExp BoolExp
+    | Or BoolExp BoolExp
+    | Not BoolExp
+
+
+type alias Procedure =
+    Nonempty Statement
+
+
+type Statement
+    = Assign Name ArithExp
+    | Increment Name ArithExp
+    | Decrement Name ArithExp
+    | If BoolExp Procedure
+    | IfElse BoolExp Procedure Procedure
+    | PreLoop BoolExp Procedure Procedure
+    | PostLoop Procedure BoolExp
+
+
+type alias DNCLProgram =
+    Procedure
 
 
 type alias Model =
