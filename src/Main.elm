eccfadbb4ec9a8519595dcdc1e33e00e9d1375bc
Yuto Takahashi <ytaka23dev@gmail.com>
2023-05-01 08:12:29 +0900

    Build UI

--- src/Main.elm
+++ src/Main.elm
@@ -6,8 +6,12 @@ import DNCL.Parser exposing (parse)
 import Debug
 import Element
     exposing
-        ( Element
+        ( Attribute
+        , Color
+        , Element
         , Option
+        , alignRight
+        , centerY
         , column
         , el
         , fill
@@ -15,15 +19,20 @@ import Element
         , height
         , layoutWith
         , maximum
+        , padding
+        , paddingXY
         , px
         , rgb255
         , row
+        , scrollbarY
+        , spacing
         , width
         )
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
 import Element.Input as Input
+import Element.Region as Region
 import Html exposing (Html)
 
 
@@ -98,6 +107,24 @@ subscriptions _ =
     Sub.none
 
 
+view : Model -> Html Msg
+view model =
+    layoutWith
+        { options = [ noFocus ] }
+        [ Background.color darkgray
+        , sansSerif
+        ]
+    <|
+        column
+            [ width fill, height fill ]
+            [ header
+            , row [ width fill, height fill ]
+                [ inputPanel model
+                , outputPanel model.output
+                ]
+            ]
+
+
 noFocus : Option
 noFocus =
     focusStyle
@@ -107,88 +134,182 @@ noFocus =
         }
 
 
-view : Model -> Html Msg
-view model =
-    layoutWith { options = [ noFocus ] } [] <|
-        column [ width fill, height fill ]
-            [ header
-            , contents model
-            , toolbar
-            ]
+white : Color
+white =
+    rgb255 0xFF 0xFF 0xFF
+
+
+black : Color
+black =
+    rgb255 0x17 0x17 0x17
+
+
+lightgray : Color
+lightgray =
+    rgb255 0x27 0x27 0x27
+
+
+darkgray : Color
+darkgray =
+    rgb255 0x1E 0x1E 0x1E
+
+
+palegray : Color
+palegray =
+    rgb255 0x3D 0x3D 0x3D
+
+
+accent : Color
+accent =
+    -- Candidates: 0x2D 0xEE 0x2C (green), 0x1A 0x9C 0xEE (blue), 0xEE 0x1A 0x8C (pink)
+    rgb255 0x2D 0xEE 0x2C
+
+
+sansSerif : Attribute msg
+sansSerif =
+    Font.family
+        [ Font.typeface "Helvetica Neue"
+        , Font.typeface "Helvetica"
+        , Font.typeface "Hiragino Sans"
+        , Font.typeface "Hiragino Kaku Gothic ProN"
+        , Font.typeface "Arial"
+        , Font.typeface "Yu Gothic"
+        , Font.typeface "Meiryo"
+        , Font.sansSerif
+        ]
+
+
+monospace : Attribute msg
+monospace =
+    Font.family
+        [ Font.typeface "Consolas"
+        , Font.typeface "Courier New"
+        , Font.typeface "Courier"
+        , Font.typeface "Monaco"
+        , Font.monospace
+        ]
 
 
 header : Element msg
 header =
     el
-        [ Background.color <| rgb255 255 100 100
+        [ Background.color black
         , width fill
-        , height <| px 75
+        , height <| px 80
+        , paddingXY 20 0
         ]
     <|
-        Element.text "Header"
+        el
+            [ Region.heading 1
+            , Font.color white
+            , Font.size 28
+            , Font.semiBold
+            , centerY
+            ]
+        <|
+            Element.text "DNCL Playground"
 
 
-contents : Model -> Element Msg
-contents model =
-    row
+inputPanel : Model -> Element Msg
+inputPanel model =
+    column
         [ width fill
         , height fill
         ]
-        [ editor model.sourceCode, output model.output ]
+        [ editor model.sourceCode
+        , toolbar
+        ]
 
 
 editor : SourceCode -> Element Msg
 editor code =
-    Input.multiline
-        [ height fill
-        , Border.width 0
-        , Font.size 18
-        , Font.family
-            [ Font.typeface "Consolas"
-            , Font.typeface "Courier New"
-            , Font.monospace
-            ]
-        ]
-    <|
-        { onChange = ReadSourceCode
-        , text = code
-        , label = Input.labelHidden "Editor"
-        , placeholder = Nothing
-        , spellcheck = False
-        }
-
-
-output : Output -> Element msg
-output out =
     el
-        [ width <| maximum 550 fill
+        [ width fill
         , height fill
-        , Font.family
-            [ Font.typeface "Consolas"
-            , Font.typeface "Courier New"
-            , Font.monospace
-            ]
+        , scrollbarY
         ]
     <|
-        Element.text <|
-            String.join "\n" <|
-                List.reverse out
+        Input.multiline
+            [ width fill
+            , height fill
+            , padding 20
+            , Border.width 0
+            , Background.color darkgray
+            , Font.color white
+            , Font.size 18
+            , monospace
+            ]
+        <|
+            { onChange = ReadSourceCode
+            , text = code
+            , label = Input.labelHidden "Editor"
+            , placeholder = Nothing
+            , spellcheck = False
+            }
 
 
 toolbar : Element Msg
 toolbar =
     el
-        [ Background.color <| rgb255 100 100 255
+        [ Background.color darkgray
+        , Border.widthEach { top = 1, bottom = 0, left = 0, right = 0 }
+        , Border.color palegray
         , width fill
-        , height <| px 75
+        , height <| px 80
+        , paddingXY 30 0
         ]
     <|
         Input.button
-            [ Background.color <| rgb255 100 255 100
+            [ alignRight
+            , centerY
+            , height <| px 50
+            , width <| px 100
+            , Border.width 1
+            , Border.color accent
+            , Border.rounded 6
+            , Font.color accent
+            , Font.size 18
+            , Font.center
             ]
             { onPress = Just RunProgram
-            , label = Element.text "Run"
+            , label = Element.text "実行"
             }
+
+
+outputPanel : Output -> Element msg
+outputPanel out =
+    column
+        [ width <| maximum 550 fill
+        , height fill
+        , Background.color lightgray
+        , Font.size 18
+        ]
+    <|
+        [ el
+            [ Region.heading 2
+            , width fill
+            , height <| px 60
+            , paddingXY 15 0
+            , Border.widthEach { top = 0, bottom = 1, left = 0, right = 0 }
+            , Border.color palegray
+            , Font.color accent
+            ]
+          <|
+            el [ centerY ] <|
+                Element.text "出力"
+        , column
+            [ width fill
+            , height fill
+            , scrollbarY
+            , padding 15
+            , spacing 6
+            , Font.color white
+            , monospace
+            ]
+          <|
+            List.map Element.text <|
+                List.reverse out
+        ]
 
 
 main : Program () Model Msg
